6f24b64a22f3ef2f0e6732fe8180fc95745a2c20
Joshua Gencianeo <146051802+Jottyyyy@users.noreply.github.com>
2025-08-25 14:10:08 +0800

    Fix typos in commit messages for consistency and clarity.

---
+++ public/build/assets/input-error-BIHy8MFy.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-CTaF9v4Y.js";import{c as o}from"./button-pPGc82xo.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
