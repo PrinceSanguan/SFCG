beb5e7935c6309280105bfed29df8a2358e84eb7
Joshua Gencianeo <146051802+Jottyyyy@users.noreply.github.com>
2025-11-15 10:24:37 +0800

    Refactor and optimize JavaScript assets across various components, enhancing performance and user experience. This update includes improvements to data handling and application efficiency, ensuring smoother interactions for users in grading functionalities, instructor assignments, and dashboard navigation.

---
+++ public/build/assets/input-error-BYLFRYxg.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-CYvxa7jT.js";import{c as o}from"./button-V-FopNJN.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
--- public/build/assets/input-error-p8bsJT8Q.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-xClj_mvu.js";import{c as o}from"./button-DoqQC-FK.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
