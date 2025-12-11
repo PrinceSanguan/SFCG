065e863d09b03e6ad53d4058adbbb4172a7f412a
hanscanete24 <hansel.canete24@gmail.com>
2025-12-11 16:45:37 +0800

    Refactor asset manifest to update file names and imports for various components, enhancing organization and consistency across the application. This includes renaming files and adjusting import paths to reflect recent changes in component structure.

---
+++ public/build/assets/layout-CcldMh08.js
@@ -0,0 +1,1 @@
+import{K as e,j as t}from"./app-Dx5wNk5j.js";import{A as n,a as i}from"./app-shell-Haj07ES7.js";import{S as o,a as d}from"./app-sidebar-Ca_fE0GI.js";function x({children:a}){const{props:r}=e(),s=(r==null?void 0:r.user)??{};return t.jsxs(n,{variant:"sidebar",children:[t.jsx(o,{user:s}),t.jsxs(i,{variant:"sidebar",children:[t.jsx(d,{user:s}),a]})]})}export{x as S};
